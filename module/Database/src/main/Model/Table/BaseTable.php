52b96ac8481a06f9b90b06bfca015fbafefc4d84
brian978 <andrei.dincescu1@gmail.com>
2015-01-06 16:45:35 +0200

    Added some helpers in the BaseTable class

--- module/Database/src/main/Model/Table/BaseTable.php
+++ module/Database/src/main/Model/Table/BaseTable.php
@@ -9,6 +9,8 @@
 
 namespace Database\Model\Table;
 
+use Aura\SqlQuery\QueryFactory;
+use Aura\SqlQuery\QueryInterface;
 use Database\Connection\ConnectionRegistry;
 
 /**
@@ -19,9 +21,30 @@ use Database\Connection\ConnectionRegistry;
 class BaseTable
 {
     /**
+     * A connection registry for the database connections
+     *
      * @var ConnectionRegistry
      */
     private static $connectionRegistry = null;
+
+    /**
+     * The database connection identifier that will be used when doing a database query
+     *
+     * @var string
+     */
+    protected $connectionName = 'default';
+
+    /**
+     * The table name for the table that this object represents
+     *
+     * @var string
+     */
+    protected $tableName = '';
+
+    /**
+     * @var QueryFactory
+     */
+    protected $queryFactory = null;
 
     /**
      * Sets the connection registry that will be used by the table models to query the database
@@ -41,8 +64,48 @@ class BaseTable
      *
      * @return ConnectionRegistry
      */
-    protected  static function getConnectionRegistry()
+    protected static function getConnectionRegistry()
     {
         return self::$connectionRegistry;
+    }
+
+    /**
+     * Creates, on demand, a query factory object
+     *
+     * @return QueryFactory
+     */
+    protected function getQueryFactory()
+    {
+        if (null === $this->queryFactory) {
+            $this->queryFactory = new QueryFactory(
+                self::getConnectionRegistry()->getConnectionSqlMode($this->connectionName)
+            );
+        }
+
+        return $this->queryFactory;
+    }
+
+    /**
+     * Executes a query along with the binded parameters and returns the statement object
+     *
+     * @param QueryInterface $query
+     * @return \PDOStatement
+     */
+    protected function executeSql(QueryInterface $query)
+    {
+        $sth = static::getConnectionRegistry()->getConnection($this->connectionName)->prepare($query->__toString());
+        $sth->execute($query->getBindValues());
+
+        return $sth;
+    }
+
+    /**
+     * Returns a select object that results in the following statement: "SELECT * FROM `$tableName`"
+     *
+     * @return \Aura\SqlQuery\Common\SelectInterface
+     */
+    protected function getSelect()
+    {
+        return $this->getQueryFactory()->newSelect()->from($this->tableName)->cols(['*']);
     }
 }
