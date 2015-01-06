dc19526ddedd4e73e5da281da6db85d4d4c80a39
brian978 <andrei.dincescu1@gmail.com>
2015-01-06 14:15:39 +0200

    Added database configuration

--- module/Database/resources/config/database.config.php
+++ module/Database/resources/config/database.config.php
@@ -7,6 +7,20 @@
  * @license https://github.com/brian978/Acamar-SkeletonApplication/blob/master/LICENSE New BSD License
  */
 
-return [
+// Need to make sure the database file exists
+$dbFile = 'data/database/bookstore.sq3';
+if (!file_exists($dbFile)) {
+    touch($dbFile);
+}
 
+// Now we can build the configuration
+return [
+    'db' => [
+        'main' => [
+            'dsn' => 'sqlite:' . $dbFile,
+            'username' => '',
+            'password' => '',
+            'options' => []
+        ]
+    ]
 ];
