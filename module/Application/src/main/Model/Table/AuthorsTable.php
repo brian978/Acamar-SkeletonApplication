9641c39fd9f659a296eeee0718095cc3737b71e1
brian978 <andrei.dincescu1@gmail.com>
2015-01-06 14:40:43 +0200

    Added an authors table model that will be used to query the authors table

---
+++ module/Application/src/main/Model/Table/AuthorsTable.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Acamar-SkeletonApplication
+ *
+ * @link https://github.com/brian978/Acamar-SkeletonApplication
+ * @copyright Copyright (c) 2014
+ * @license https://github.com/brian978/Acamar-SkeletonApplication/blob/master/LICENSE New BSD License
+ */
+
+namespace Application\Model\Table;
+
+use Database\Model\Table\BaseTable;
+
+/**
+ * Class Authors
+ *
+ * @package Application\Model\Table
+ */
+class AuthorsTable extends BaseTable
+{
+
+}
--- module/Database/src/main/Setup.php
+++ module/Database/src/main/Setup.php
@@ -62,6 +62,5 @@ class Setup
      */
     public function initializeSchema(MvcEvent $event)
     {
-
     }
 }
